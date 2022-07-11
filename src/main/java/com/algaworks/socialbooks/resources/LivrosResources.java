edf5c86d145bdc37fb170c31143acccd3856bfaf
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-07-11 17:56:59 -0300

    Feito parte das tratativas de retorno do delete e put

--- src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,8 +26,8 @@ public class LivrosResources {
 	private LivrosRepository livrosRepository; // instanciando o Repository que faz acesso aos dados
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public List<Livro> listar() {
-		return livrosRepository.findAll(); // Utilizado do Repository/DAO instanciado para retornar tudo da Livros
+	public ResponseEntity<List<Livro>> listar() {
+		return ResponseEntity.status(HttpStatus.OK).body(livrosRepository.findAll()); // Utilizado do Repository/DAO instanciado para retornar tudo da Livros
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
@@ -53,14 +54,20 @@ public class LivrosResources {
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public void deletar(@PathVariable("id")Long id) {
-		livrosRepository.deleteById(id);
+	public ResponseEntity<Void> deletar(@PathVariable("id")Long id) {
+		try {
+			livrosRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.noContent().build();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public void atualizar(@RequestBody Livro livro, @PathVariable("id")Long id) {
+	public ResponseEntity<Void> atualizar(@RequestBody Livro livro, @PathVariable("id")Long id) {
 		livro.setId(id);
 		livrosRepository.save(livro);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
