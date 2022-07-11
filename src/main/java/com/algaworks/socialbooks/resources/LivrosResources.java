bf9ed76c971189dce112f531a8d931c72ef608bf
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-07-11 10:46:09 -0300

    Implementado method POST

--- src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -3,6 +3,7 @@ package com.algaworks.socialbooks.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +12,19 @@ import com.algaworks.socialbooks.domain.Livro;
 import com.algaworks.socialbooks.repository.LivrosRepository;
 
 @RestController
+@RequestMapping("/livros")
 public class LivrosResources {
 	
 	@Autowired
 	private LivrosRepository livrosRepository; // instanciando o Repository que faz acesso aos dados
 	
-	@RequestMapping(value = "/livros", method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public List<Livro> listar() {
 		return livrosRepository.findAll(); // Utilizado do Repository/DAO instanciado para retornar tudo da Livros
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public void salvar(@RequestBody Livro livro) { //@RequestBody usado para pegar requisição e colocar no objeto
+		livrosRepository.save(livro);
 	}
 }
